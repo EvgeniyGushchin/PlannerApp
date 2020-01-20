dd007c9de66ac135fbab28f9ff23b1b4343ac789
Evgeniy Gushchin <evgeniy.gushchin@aldigit.com>
2020-01-20 11:01:07 +0600

    create new xcode project from template

---
+++ Planner/Planner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Planner
+//
+//  Created by Evgeniy Gushchin on 20.01.2020.
+//  Copyright © 2020 EG. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
